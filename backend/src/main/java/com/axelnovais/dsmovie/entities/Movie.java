ab0c6646cf7a4ba4421ae0f22972397456c48879
FelipeAxel <axelnovais96@gmail.com>
2022-05-11 11:17:08 -0300

    Domain-model-database-seed

--- backend/src/main/java/com/axelnovais/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/axelnovais/dsmovie/entities/Movie.java
@@ -1,6 +1,17 @@
 package com.axelnovais.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_movie")
 public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private Double score;
@@ -18,7 +29,8 @@ public class Movie {
 		this.count = count;
 		this.image = image;
 	}
-
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -26,35 +38,35 @@ public class Movie {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getTitle() {
 		return title;
 	}
-
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	
 	public Double getScore() {
 		return score;
 	}
-
+	
 	public void setScore(Double score) {
 		this.score = score;
 	}
-
+	
 	public Integer getCount() {
 		return count;
 	}
-
+	
 	public void setCount(Integer count) {
 		this.count = count;
 	}
-
+	
 	public String getImage() {
 		return image;
 	}
-
+	
 	public void setImage(String image) {
 		this.image = image;
 	}
--- backend/src/main/java/com/axelnovais/dsmovie/entities/User.java
+++ backend/src/main/java/com/axelnovais/dsmovie/entities/User.java
@@ -1,7 +1,17 @@
 package com.axelnovais.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="tb_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
 	
@@ -30,6 +40,5 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
 	
 }
